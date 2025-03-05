cbd8d4dafa72bb47c72590121eb3aaca264581d8
aiden <aidenlin@star-link.tech>
2025-03-05 17:10:13 +0800

    add connectionManager

--- IM/src/main/java/com/cooba/core/spring/JwtHandshakeInterceptor.java
+++ IM/src/main/java/com/cooba/core/spring/JwtHandshakeInterceptor.java
@@ -2,32 +2,60 @@ package com.cooba.core.spring;
 
 import com.cooba.constant.ErrorEnum;
 import com.cooba.exception.BaseException;
+import com.cooba.util.ConnectionManager;
 import com.cooba.util.JwtHeaderValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class JwtHandshakeInterceptor implements ChannelInterceptor {
     private final JwtHeaderValidator jwtHeaderValidator;
+    private final ConnectionManager connectionManager;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String sessionId = accessor.getSessionId();
+        String user = Objects.requireNonNull(accessor.getUser()).getName();
+        String destination = accessor.getDestination();
+        System.out.println(accessor.getCommand() + destination);
+        switch (Objects.requireNonNull(accessor.getCommand())) {
+            case CONNECT -> {
+                String authToken = accessor.getFirstNativeHeader("Authorization");
 
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            String authToken = accessor.getFirstNativeHeader("Authorization");
-
-            Long userId = jwtHeaderValidator.validHeader(authToken);
-            if (!userId.toString().equals(accessor.getUser().getName())) {
-                throw new BaseException(ErrorEnum.INVALID_AUTHORIZATION);
+                Long userId = jwtHeaderValidator.validHeader(authToken);
+                if (!userId.toString().equals(user)) {
+                    throw new BaseException(ErrorEnum.INVALID_AUTHORIZATION);
+                }
+                connectionManager.addUser(user, sessionId);
+            }
+            case DISCONNECT -> {
+                connectionManager.removeUser(user, sessionId);
+            }
+            case SUBSCRIBE -> {
+                assert destination != null;
+                boolean group = destination.startsWith("group");
+                if (group) {
+                    String roomId = destination.split("/")[1];
+                    connectionManager.addGroupUser(roomId, user, sessionId);
+                }
+            }
+            case UNSUBSCRIBE -> {
+                assert destination != null;
+                boolean group = destination.startsWith("group");
+                if (group) {
+                    String roomId = destination.split("/")[1];
+                    connectionManager.removeGroupUser(roomId, user, sessionId);
+                }
             }
         }
         return message;
