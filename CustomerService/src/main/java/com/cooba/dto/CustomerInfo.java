1cb3114b07ab5b0940ce04c8e77489b4a7fd0e9f
aiden <aidenlin@star-link.tech>
2025-03-13 10:57:14 +0800

    add CustomerInfo roomId

--- CustomerService/src/main/java/com/cooba/dto/CustomerInfo.java
+++ CustomerService/src/main/java/com/cooba/dto/CustomerInfo.java
@@ -8,10 +8,12 @@ public class CustomerInfo {
     private long agentCustomerId;
     private long customerUserId;
     private String name;
+    private long roomId;
 
     public CustomerInfo(AgentCustomer agentCustomer) {
         this.agentCustomerId = agentCustomer.getId();
         this.customerUserId = agentCustomer.getCustomerUserId();
         this.name = agentCustomer.getShowName();
+        this.roomId = agentCustomer.getRoomId();
     }
 }
