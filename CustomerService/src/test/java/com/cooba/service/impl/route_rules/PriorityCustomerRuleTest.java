bf25c83f8aff053d4f7523a6722cc35436f87b2b
aiden <aidenlin@star-link.tech>
2025-03-14 15:09:08 +0800

    add rule test

--- CustomerService/src/test/java/com/cooba/service/impl/route_rules/PriorityCustomerRuleTest.java
+++ CustomerService/src/test/java/com/cooba/service/impl/route_rules/PriorityCustomerRuleTest.java
@@ -1,12 +1,41 @@
 package com.cooba.service.impl.route_rules;
 
+import com.cooba.entity.Agent;
+import com.cooba.entity.AgentCustomer;
+import com.cooba.entity.User;
+import com.cooba.repository.AgentCustomerRepository;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {PriorityCustomerRule.class})
 class PriorityCustomerRuleTest {
+    @Autowired
+    PriorityCustomerRule priorityCustomerRule;
+    @MockitoBean
+    AgentCustomerRepository agentCustomerRepository;
 
     @Test
     void filterAgent() {
+        List<Agent> agents = Instancio.createList(Agent.class);
+        User customer = Instancio.create(User.class);
+
+        AgentCustomer agentCustomer = Instancio.create(AgentCustomer.class);
+        agentCustomer.setAgentUserId(agents.get(0).getUserId());
+        agentCustomer.setCustomerUserId(customer.getId());
+        Mockito.when(agentCustomerRepository.findByCustomerId(customer.getId())).thenReturn(List.of(agentCustomer));
+
+        List<Agent> filterAgent = priorityCustomerRule.filterAgent(agents, customer);
+        Assertions.assertFalse(filterAgent.isEmpty());
+        Assertions.assertEquals(agents.get(0).getUserId(), filterAgent.get(0).getUserId());
     }
 }
--- CustomerService/src/test/java/com/cooba/service/impl/route_rules/RandomAgentRuleTest.java
+++ CustomerService/src/test/java/com/cooba/service/impl/route_rules/RandomAgentRuleTest.java
@@ -1,12 +1,39 @@
 package com.cooba.service.impl.route_rules;
 
+import com.cooba.entity.Agent;
+import com.cooba.entity.User;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {RandomAgentRule.class})
 class RandomAgentRuleTest {
+    @Autowired
+    RandomAgentRule randomAgentRule;
 
     @Test
     void filterAgent() {
+        List<Agent> agents = Instancio.createList(Agent.class);
+        User customer = Instancio.create(User.class);
+
+        Map<Long, Integer> agentCountMap = new HashMap<>();
+        for (int i = 0; i < 100; i++) {
+            List<Agent> filterAgent = randomAgentRule.filterAgent(agents, customer);
+            Assertions.assertEquals(1, filterAgent.size());
+
+            agentCountMap.compute(filterAgent.get(0).getId(), (k, v) -> (v == null) ? 1 : v + 1);
+        }
+        agentCountMap.forEach((k, v) -> Assertions.assertTrue(v < 100));
+
     }
 }
