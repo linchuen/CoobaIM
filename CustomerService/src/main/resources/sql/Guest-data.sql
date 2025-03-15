d44a9d1c923e6333480d5595af895db9cfd3abc4
Aiden <x29587629@gmail.com>
2025-03-15 15:29:01 +0800

    add guest data

---
+++ CustomerService/src/main/resources/sql/Guest-data.sql
@@ -0,0 +1,7 @@
+INSERT INTO im.t_guest
+(id, created_time, user_id)
+VALUES(1, NOW(), 3);
+
+INSERT INTO im.t_user
+(id, created_time, email, name, password, `role`)
+VALUES(3,  NOW(), 'guest@example.com', 'guest', '084e0343a0486ff05530df6c705c8bb4', 'ROLE_GUEST');
