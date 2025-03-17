49fbd20678324c915409458eca894c7f7351eb49
aiden <aidenlin@star-link.tech>
2025-03-17 13:15:26 +0800

    add channel default sql

---
+++ CustomerService/src/main/resources/sql/OfficialChannel-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO im.t_official_channel (created_time, is_public, name) VALUES(NOW(), 1, '24H客服');
+INSERT INTO im.t_official_channel (created_time, is_public, name) VALUES(NOW(), 0, '會員客服');
