29357ba9d10f18346ccb0db5ca40b3ad87c8ff99
Aiden <x29587629@gmail.com>
2025-04-05 23:13:53 +0800

    update sql

--- IM/src/main/resources/sql/clickhouse/ChatSearch-schema.sql
+++ IM/src/main/resources/sql/clickhouse/ChatSearch-schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE t_chat_search (
+CREATE TABLE im.t_chat_search (
     id UInt64 NOT NULL,
     room_id Int64 NOT NULL,
     message_gram String NOT NULL,
@@ -9,5 +9,5 @@ ENGINE = MergeTree()
 PARTITION BY toYYYYMM(created_time)
 ORDER BY (id);
 
-ALTER TABLE t_chat_search ADD PROJECTION proj_uk_roomId
+ALTER TABLE im.t_chat_search ADD PROJECTION proj_uk_roomId
 ( SELECT  *  ORDER BY room_id, message_gram, chat_id, id );
