0e91f0ceb1e44094ffe292a461740a011ee71500
Aiden <x29587629@gmail.com>
2025-04-06 20:30:17 +0800

    update data

--- CustomerService/src/main/resources/sql/Agent-data.sql
+++ CustomerService/src/main/resources/sql/Agent-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO im.t_user
-(id, created_time, email, name, password, `role`)
-VALUES(2, NOW(), 'agent@example.com', 'agent', 'b33aed8f3134996703dc39f9a7c95783', 'ROLE_AGENT');
+(id, created_time, email, name, password, `role`, avatar)
+VALUES(2, NOW(), 'agent@example.com', 'agent', 'b33aed8f3134996703dc39f9a7c95783', 'ROLE_AGENT', '');
 
 INSERT INTO im.t_agent
 (id, created_time, department, is_default, is_disable, name, updated_time, user_id)
